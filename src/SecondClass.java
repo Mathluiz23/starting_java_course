b927ed5502f408196d5ba49b544eaf919ba8820f
Mathluiz23 <matheus_luizs@hotmail.com>
2024-06-11 16:37:03 +1200

    primeira class

---
+++ src/SecondClass.java
@@ -0,0 +1,8 @@
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, Matheus");
+        System.out.print("como vai?");
+        System.out.println("  " + "estou bem, obrigado!");
+
+    }
+}
